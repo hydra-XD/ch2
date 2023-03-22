fe44700319a8efbded24253b21ed3bc4b1addef3
Chloe Boyer <waterhydramc@gmail.com>
2023-03-22 18:54:44 -0400

    Update game.js

--- dev/js/game.js
+++ dev/js/game.js
@@ -343,7 +343,9 @@ var Game = Class.extend({
 
         var self = this;
 
-        var time = ~~(((new Date()).getTime() - this.stats.starttime));
+        var time = 2.3
+
+       // var time = ~~(((new Date()).getTime() - this.stats.starttime));
 
         ga('send', 'event', 'finish level', this.currentLevelId, time);
 
